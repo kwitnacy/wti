795710dec07190a3c044576308d0e2ceb9ec5dea
piotr kwiatkowski - kwitnacy <piotr.d.kwiatkowski@student.put.poznan.pl>
2020-04-15 12:54:18 +0200

    :)

--- wtiproj05/API_logic.py
+++ wtiproj05/API_logic.py
@@ -1,16 +1,15 @@
 from redis import Redis
+from typing import List
 import statistics as stat
 import pandas as pd
 import numpy as np
 import json
 import sys
 
-import API as _API
-
 
 class API():
 	def __init__(self):
-		#self.genres = _API.my_join()
+		#self.genres = self.my_join()
 		self.genres = ['Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'IMAX', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Short', 'Thriller', 'War', 'Western']
 		self.head = ['userID', 'movieID', 'rating', 'date_day', 'date_month', 'date_year', 'date_hour', 'date_minute' ,'date_second', 'Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'IMAX', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Short', 'Thriller', 'War', 'Western']
 		self.data = pd.read_csv('/home/kwitnoncy/Documents/politechnika/wti/wtiproj05/data/joined.dat', sep='\t')
@@ -28,7 +27,7 @@ class API():
 
 		self.data[np.isnan] = 0
 
-		temp_avg = _API.get_avg_rating_genre(self.data)
+		temp_avg = self.get_avg_rating_genre()
 		self.avg_per_gerne = np.array(temp_avg[0])
 		self.count_per_gerne = np.array(temp_avg[1])
 
@@ -103,9 +102,38 @@ class API():
 	
 
 	def get_user_profile(self, user_ID) -> dict:
-		profile = np.array(self.avg_per_gerne) - _API.get_avg_rating_genre_user(data=self.data, user_ID=user_ID)
+		profile = np.array(self.avg_per_gerne) - self.get_avg_rating_genre_user(user_ID=user_ID)
 		
 		ret = {x: y for x, y in zip(self.genres, profile)}
 		ret['userID'] = user_ID
 
 		return json.dumps(ret)
+	
+	def my_join(self) -> List[str]:
+		df_main = pd.read_csv('/home/kwitnoncy/Documents/politechnika/wti/wtiproj05/data/user_ratedmovies.dat', sep='\t')
+		df_genres = pd.read_csv('/home/kwitnoncy/Documents/politechnika/wti/wtiproj05/data/movie_genres.dat', sep='\t')
+		df_genres['Dummy'] = 1
+
+		df_pivoted = df_genres.pivot_table(index='movieID', columns='genre', values='Dummy')
+		joined = df_main.join(df_pivoted, on='movieID')
+		joined[np.isnan] = 0
+		joined.to_csv('/home/kwitnoncy/Documents/politechnika/wti/wtiproj03/data/joined.dat', index=False, sep='\t')
+
+		return list(df_pivoted.columns.values)
+
+	def get_avg_rating_genre(self) -> (np.array, np.array):
+		joined = self.data.to_numpy()
+
+		avg = [np.nanmean([(row[2] * row[genre + 9]) if row[genre + 9] != 0 else np.nan for row in joined]) for genre in range(len(self.genres))]
+
+		pd.DataFrame([avg], columns=self.genres).to_csv('/home/kwitnoncy/Documents/politechnika/wti/wtiproj04/data/avg_rating.dat', index=False, sep='\t')
+		
+		return np.array(avg), np.array([np.count_nonzero(col) for col in joined.T[9:]])
+
+	def get_avg_rating_genre_user(self, user_ID: int = 0) -> np.array:
+		query = 'userID == ' + str(user_ID)
+		
+		data = data.query(query).to_numpy()
+
+		return np.array([np.nanmean([(row[2] * row[genre + 9]) if row[genre + 9] != 0 else np.nan for row in data]) for genre in range(len(self.genres))])
+
